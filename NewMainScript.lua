a6c3b5a519eaacd2e2dcc618c832e3cca74b647f
7GrandDad <granddad7-neew@outlook.com>
2022-11-09 10:51:47 -0700

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,16 +1,26 @@
 local websocketfunc = syn and syn.websocket and syn.websocket.connect or Krnl and Krnl.WebSocket.connect or WebSocket and WebSocket.connect or websocket and websocket.connect
-local suc, web = pcall(function() return websocketfunc("ws://127.0.0.1:6892/") end)
+local suc, web
 if syn and syn.toast_notification then 
     suc, web = pcall(function() 
         local socket = WebsocketClient.new("ws://127.0.0.1:6892/")
         socket:Connect()
         return socket 
     end)
+else
+    suc, web = pcall(function() return websocketfunc("ws://127.0.0.1:6892/") end)
 end
 repeat 
     task.wait(1)
     if not suc or suc and type(web) == "boolean" then
-        suc, web = pcall(function() return websocketfunc("ws://127.0.0.1:6892/") end)
+        if syn and syn.toast_notification then 
+            suc, web = pcall(function() 
+                local socket = WebsocketClient.new("ws://127.0.0.1:6892/")
+                socket:Connect()
+                return socket 
+            end)
+        else
+            suc, web = pcall(function() return websocketfunc("ws://127.0.0.1:6892/") end)
+        end
         if not suc or suc and type(web) == "boolean" then
             print("websocket error:", web)
         end
