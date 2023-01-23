78b596bb982efdb51c4595be80d3c2394404d8ed
7GrandDad <grand7dadd@outlook.com>
2023-01-23 13:28:39 -0700

    removed

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,6 +1,5 @@
 local websocketfunc = syn and syn.websocket and syn.websocket.connect or Krnl and Krnl.WebSocket.connect or WebSocket and WebSocket.connect or websocket and websocket.connect
 local suc, web
---testing [$Id$]
 if syn and syn.toast_notification then 
     suc, web = pcall(function() 
         local socket = WebsocketClient.new("ws://127.0.0.1:6892/")
