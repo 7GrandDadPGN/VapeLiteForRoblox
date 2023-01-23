dcc52d25f84f31823b600fa30365f1ecd38408f9
7GrandDad <grand7dadd@outlook.com>
2023-01-23 14:17:58 -0700

    ok

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,6 +1,6 @@
 local websocketfunc = syn and syn.websocket and syn.websocket.connect or Krnl and Krnl.WebSocket.connect or WebSocket and WebSocket.connect or websocket and websocket.connect
 local suc, web
---here we go again3
+
 if syn and syn.toast_notification then 
     suc, web = pcall(function() 
         local socket = WebsocketClient.new("ws://127.0.0.1:6892/")
